3b7f005f696e9ab2730064f1d01f62053bd23119
vriverh <vriverh@gmail.com>
2018-04-25 11:24:44 +0800

    完成了Button

---
+++ AvaloniaDemoApp/Control/ButtonDemo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemoApp.Control
+{
+    public class ButtonDemo : UserControl
+    {
+        public ButtonDemo()
+        {
+            this.InitializeComponent();
+            this.FindControl<Button>("button1").Click += (s, e) =>
+            {
+                this.FindControl<TextBlock>("txt").Text = "Button1 is Click";
+            };
+            this.FindControl<Button>("button2").Click += (s, e) =>
+            {
+                this.FindControl<TextBlock>("txt").Text = "Button2 is Click";
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
