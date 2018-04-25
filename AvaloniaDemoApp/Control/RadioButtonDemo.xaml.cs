9f0da7df70dccc7fbb1b6706ae8a3f854e617204
vriverh <vriverh@gmail.com>
2018-04-25 15:22:16 +0800

    新增了CheckBox和RadioButton

---
+++ AvaloniaDemoApp/Control/RadioButtonDemo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemoApp.Control
+{
+    public class RadioButtonDemo : UserControl
+    {
+        public RadioButtonDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
