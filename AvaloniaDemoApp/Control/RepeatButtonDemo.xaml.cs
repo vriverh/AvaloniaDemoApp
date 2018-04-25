e70c86828eaea91d1bc65c8a7aa252b4a8f95709
vriverh <vriverh@gmail.com>
2018-04-25 14:44:01 +0800

    添加了RepeatButton和ToggleButton

---
+++ AvaloniaDemoApp/Control/RepeatButtonDemo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemoApp.Control
+{
+    public class RepeatButtonDemo : UserControl
+    {
+        private int _clickNumber = 0;
+
+        public RepeatButtonDemo()
+        {
+            this.InitializeComponent();
+            this.FindControl<Button>("fastRepeatButton").Click += RepeatButtonDemo_Click;
+            this.FindControl<Button>("slowRepeatButton").Click += RepeatButtonDemo_Click;
+        }
+
+        private void RepeatButtonDemo_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.FindControl<TextBlock>("clickNumberTxt").Text = _clickNumber++.ToString();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ AvaloniaDemoApp/Control/ToggleButtonDemo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemoApp.Control
+{
+    public class ToggleButtonDemo : UserControl
+    {
+        public ToggleButtonDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
