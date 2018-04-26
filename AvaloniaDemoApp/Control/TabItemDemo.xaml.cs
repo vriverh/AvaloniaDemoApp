dd420a83170d70e077c99879036634a38b1dd7b7
vriverh <vriverh@gmail.com>
2018-04-26 11:43:22 +0800

    完成了Expander和TabItem

---
+++ AvaloniaDemoApp/Control/TabItemDemo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemoApp.Control
+{
+    public class TabItemDemo : UserControl
+    {
+        public TabItemDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
